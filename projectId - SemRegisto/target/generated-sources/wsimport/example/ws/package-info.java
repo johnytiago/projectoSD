85f6e10a6100300e483138d10d9cbef7be215660
johnytiago <joaosao10@gmail.com>
2015-04-09 17:27:25 +0100

    only a proj

--- projectId - SemRegisto/target/generated-sources/wsimport/example/ws/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.example/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package example.ws;
--- projectoID/target/generated-sources/wsimport/pt/ulisboa/tecnico/sdis/id/ws/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:pt:ulisboa:tecnico:sdis:id:ws")
-package pt.ulisboa.tecnico.sdis.id.ws;
