362777e17ec51557c50fdb58b0b5648f495335e5
johnytiago <joaosao10@gmail.com>
2015-04-10 12:56:24 +0100

    added initial user for testing

--- projectoID/src/main/java/id/ws/impl/Sd_IdImpl.java
+++ projectoID/src/main/java/id/ws/impl/Sd_IdImpl.java
@@ -24,11 +24,27 @@ import java.util.HashMap;
 )
 public class Sd_IdImpl implements SDId {
     HashMap<String, User> usersLog = new HashMap<String, User>();
+
+    public Sd_IdImpl(){
+        User alice = new User("alice", "alice@tecnico.pt", "Aaa1");
+        usersLog.put("alice", alice);
+        User bruno = new User("bruno", "bruno@tecnico.pt", "Bbb2");
+        usersLog.put("bruno", bruno);
+        User carla = new User("carla", "carla@tecnico.pt", "Ccc3");
+        usersLog.put("carla", carla);
+        User duarte = new User("duarte", "duarte@tecnico.pt", "Ddd4");
+        usersLog.put("duarte", duarte);
+        User eduardo = new User("eduardo", "eduardo@tecnico.pt", "Eee5");
+        usersLog.put("eduardo", eduardo);
+    }
 	
 	public void createUser(String userId, String emailAddress) 
             throws EmailAlreadyExists_Exception, InvalidEmail_Exception, InvalidUser_Exception, UserAlreadyExists_Exception{
 
-        //perguntar a opinião do prof acerca disto
+        for (User user : usersLog) {
+            
+        }
+
         SecureRandom SECURE_RANDOM = new SecureRandom();
         String pass = new BigInteger(130, SECURE_RANDOM).toString(32);
         User user = new User(userId, emailAddress, pass);
@@ -42,7 +58,7 @@ public class Sd_IdImpl implements SDId {
         if(u == null){
             UserDoesNotExist faultInfo = new UserDoesNotExist();
             faultInfo.setUserId(userId);
-            throw new UserDoesNotExist_Exception("User does not exist.", faultInfo);
+            throw new UserDoesNotExist_Exception("The user: " + userID + " does not exist.", faultInfo);
         }
         else{
             SecureRandom SECURE_RANDOM = new SecureRandom();
@@ -55,8 +71,15 @@ public class Sd_IdImpl implements SDId {
 
     public void removeUser(String userId) 
             throws UserDoesNotExist_Exception{
-        usersLog.remove(userId);
-
+        User u = usersLog.get(userId);
+        if(u == null){
+            UserDoesNotExist faultInfo = new UserDoesNotExist();
+            faultInfo.setUserId(userId);
+            throw new UserDoesNotExist_Exception("The user you are trying to remove does not exists.", faultInfo);
+        }
+        else{
+            usersLog.remove(userId);
+        }
     }
 
     public byte[] requestAuthentication(String userId, byte[] reserved)
